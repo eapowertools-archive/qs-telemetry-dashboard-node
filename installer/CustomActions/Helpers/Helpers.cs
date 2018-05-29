27d440d9fe6f3f8a3988590527cf52a7b6a9d667
Jesse Paris <jparis@gmail.com>
2018-05-28 19:46:31 -0700

    adding all files

---
+++ installer/CustomActions/Helpers/Helpers.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomActions.Helpers
+{
+	public enum PemStringType
+	{
+		Certificate,
+		RsaPrivateKey
+	}
+
+	internal class HelperFunctions
+	{
+		/// <summary>
+		/// This helper function parses an integer size from the reader using the ASN.1 format
+		/// </summary>
+		/// <param name="rd"></param>
+		/// <returns></returns>
+		public static int DecodeIntegerSize(System.IO.BinaryReader rd)
+		{
+			byte byteValue;
+			int count;
+
+			byteValue = rd.ReadByte();
+			if (byteValue != 0x02)        // indicates an ASN.1 integer value follows
+				return 0;
+
+			byteValue = rd.ReadByte();
+			if (byteValue == 0x81)
+			{
+				count = rd.ReadByte();    // data size is the following byte
+			}
+			else if (byteValue == 0x82)
+			{
+				byte hi = rd.ReadByte();  // data size in next 2 bytes
+				byte lo = rd.ReadByte();
+				count = BitConverter.ToUInt16(new[] { lo, hi }, 0);
+			}
+			else
+			{
+				count = byteValue;        // we already have the data size
+			}
+
+			//remove high order zeros in data
+			while (rd.ReadByte() == 0x00)
+			{
+				count -= 1;
+			}
+			rd.BaseStream.Seek(-1, System.IO.SeekOrigin.Current);
+
+			return count;
+		}
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="pemString"></param>
+		/// <param name="type"></param>
+		/// <returns></returns>
+		public static byte[] GetBytesFromPEM(string pemString, PemStringType type)
+		{
+			string header; string footer;
+
+			switch (type)
+			{
+				case PemStringType.Certificate:
+					header = "-----BEGIN CERTIFICATE-----";
+					footer = "-----END CERTIFICATE-----";
+					break;
+				case PemStringType.RsaPrivateKey:
+					header = "-----BEGIN RSA PRIVATE KEY-----";
+					footer = "-----END RSA PRIVATE KEY-----";
+					break;
+				default:
+					return null;
+			}
+
+			int start = pemString.IndexOf(header) + header.Length;
+			int end = pemString.IndexOf(footer, start) - start;
+			return Convert.FromBase64String(pemString.Substring(start, end));
+		}
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="inputBytes"></param>
+		/// <param name="alignSize"></param>
+		/// <returns></returns>
+		public static byte[] AlignBytes(byte[] inputBytes, int alignSize)
+		{
+			int inputBytesSize = inputBytes.Length;
+
+			if ((alignSize != -1) && (inputBytesSize < alignSize))
+			{
+				byte[] buf = new byte[alignSize];
+				for (int i = 0; i < inputBytesSize; ++i)
+				{
+					buf[i + (alignSize - inputBytesSize)] = inputBytes[i];
+				}
+				return buf;
+			}
+			else
+			{
+				return inputBytes;      // Already aligned, or doesn't need alignment
+			}
+		}
+	}
+}
--- installer/CustomActions/QrsCustomActions.cs
+++ installer/CustomActions/QrsCustomActions.cs
@@ -1,16 +1,250 @@
 ﻿using System.IO;
 using Microsoft.Deployment.WindowsInstaller;
+using System.Security.Cryptography.X509Certificates;
+using System.Net;
+using System;
+using Newtonsoft.Json;
+using CustomActions.Helpers;
+using System.Security.Cryptography;
+using Newtonsoft.Json.Linq;
 
 namespace CustomActions
 {
+	enum HTTPMethod
+	{
+		GET,
+		PUT,
+		POST,
+		DELETE
+	}
+
+	enum HTTPContentType
+	{
+		json,
+		app
+	}
+
 	public class QrsCustomActions
 	{
-		[CustomAction]
-		public static ActionResult CustomAction1(Session session)
+		private static X509Certificate2 SENSE_CERT = GetCertificate();
+
+		private static Tuple<HttpStatusCode, string> MakeQrsRequest(string path, HTTPMethod method, HTTPContentType contentType = HTTPContentType.json, byte[] body = null)
 		{
-			File.Create(@"c:\installed.txt");
+			// Fix Path
+			if (!path.StartsWith("/"))
+			{
+				path = '/' + path;
+			}
+			if (path.EndsWith("/"))
+			{
+				path = path.Substring(0, path.Length - 1);
+			}
+			int indexOfSlash = path.LastIndexOf('/');
+			int indexOfQuery = path.LastIndexOf('?');
+			if (indexOfQuery <= indexOfSlash)
+			{
+				path += "?";
+			}
+			else
+			{
+				path += "&";
+			}
+						
+			string responseString = "";
+			HttpStatusCode responseCode = 0;
+			ServicePointManager.ServerCertificateValidationCallback = delegate { return true; };
+
+			//Create the HTTP Request and add required headers and content in xrfkey
+			string xrfkey = "0123456789abcdef";
+			HttpWebRequest request = (HttpWebRequest)WebRequest.Create(@"https://localhost:4242/qrs" + path + "xrfkey=" + xrfkey);
+			request.Method = method.ToString();
+			request.Accept = "application/json";
+			request.Headers.Add("X-Qlik-xrfkey", xrfkey);
+			request.Headers.Add("X-Qlik-User", @"UserDirectory=internal;UserId=sa_api");
+			// Add the certificate to the request and provide the user to execute as
+			request.ClientCertificates.Add(SENSE_CERT);
+
+			if (method == HTTPMethod.POST || method == HTTPMethod.PUT)
+			{
+				// Set Headers
+				if (contentType == HTTPContentType.json)
+				{
+					request.ContentType = "application/json";
+					
+				}
+				else if (contentType == HTTPContentType.app)
+				{
+					request.ContentType = "application/vnd.qlik.sense.app";
+				}
+				else
+				{
+					throw new ArgumentException("Content type '" + contentType.ToString() + "' is not supported.");
+				}
+
+				// Set Body
+				if (body == null)
+				{
+					request.ContentLength = 0;
+				}
+				else
+				{
+					request.ContentLength = body.Length;
+					Stream requestStream = request.GetRequestStream();
+					requestStream.Write(body, 0, body.Length);
+					requestStream.Close();
+				}
+			}
+
+			// make the web request and return the content
+			try
+			{
+				using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+				{
+					responseCode = response.StatusCode;
+					using (Stream stream = response.GetResponseStream())
+					{
+						using (StreamReader reader = new StreamReader(stream))
+						{
+							// if response is invalid, throw exception and expect it to be handled in calling function (4xx errors)
+							responseString = JsonConvert.SerializeObject(reader.ReadToEnd());
+						}
+					}
+				}
+			}
+			catch (WebException webEx)
+			{
+				return new Tuple<HttpStatusCode, string>(HttpStatusCode.ServiceUnavailable, webEx.Message);
+			}
+			catch (Exception e)
+			{
+				return new Tuple<HttpStatusCode, string>(HttpStatusCode.InternalServerError, e.Message);
+
+			}
+
+			return new Tuple<HttpStatusCode, string>(responseCode, responseString);
+		}
+
+
+		[CustomAction]
+		public static ActionResult IsRepositoryRunning(Session session)
+		{
+			Tuple<HttpStatusCode, string> response = MakeQrsRequest("/about", HTTPMethod.GET);
+			if (response.Item1 == HttpStatusCode.OK)
+			{
+				return ActionResult.Success;
+			}
+			else
+			{
+				return ActionResult.Failure;
+			}
+		}
+
+		[CustomAction]
+		public static ActionResult ImportApp(Session session)
+		{
+			Tuple<HttpStatusCode, string> hasAppResponse = MakeQrsRequest("/app/count?filter=name eq 'Telemetry Dashboard'", HTTPMethod.GET);
+			if (hasAppResponse.Item1 != HttpStatusCode.OK)
+			{
+				return ActionResult.Failure;
+			}
+
+			if (JObject.Parse((string)JsonConvert.DeserializeObject(hasAppResponse.Item2))["value"].ToObject<int>() == 1)
+			{
+				Console.WriteLine("App already exists.");
+				return ActionResult.NotExecuted;
+			}
+
+			// import app
+			Tuple<HttpStatusCode, string> importAppResponse = MakeQrsRequest("/app/upload?name=Telemetry Dashboard", HTTPMethod.POST, HTTPContentType.app, Properties.Resources.Telemetry_Dashboard);
+			Console.WriteLine(importAppResponse.Item1);
+			Console.WriteLine(importAppResponse.Item2);
+			return ActionResult.Success;
+		}
+
+		[CustomAction]
+		public static ActionResult ImportExtensions(Session session)
+		{
+			// Extension 1
+			Tuple<HttpStatusCode, string> hasExtension1 = MakeQrsRequest("/extension/count?filter=name eq 'Telemetry Dashboard'", HTTPMethod.GET);
+			if (hasExtension1.Item1 != HttpStatusCode.OK)
+			{
+				return ActionResult.Failure;
+			}
+
+			if (JObject.Parse((string)JsonConvert.DeserializeObject(hasExtension1.Item2))["value"].ToObject<int>() == 0)
+			{
+				Tuple<HttpStatusCode, string> importExtensionResponse = MakeQrsRequest("/extension/upload", HTTPMethod.POST, HTTPContentType.json, Properties.Resources.Telemetry_Dashboard);
+				if (importExtensionResponse.Item1 != HttpStatusCode.Created)
+				{
+					return ActionResult.Failure;
+				}
+			}
+
+			// Extension 2
+			Tuple<HttpStatusCode, string> hasExtension2 = MakeQrsRequest("/extension/count?filter=name eq 'Telemetry Dashboard'", HTTPMethod.GET);
+			if (hasExtension1.Item1 != HttpStatusCode.OK)
+			{
+				return ActionResult.Failure;
+			}
+
+			if (JObject.Parse((string)JsonConvert.DeserializeObject(hasExtension1.Item2))["value"].ToObject<int>() == 0)
+			{
+				Tuple<HttpStatusCode, string> importExtensionResponse = MakeQrsRequest("/extension/upload", HTTPMethod.POST, HTTPContentType.json, Properties.Resources.Telemetry_Dashboard);
+				if (importExtensionResponse.Item1 != HttpStatusCode.Created)
+				{
+					return ActionResult.Failure;
+				}
+			}
+
+			// Extension 3
+			Tuple<HttpStatusCode, string> hasExtension3 = MakeQrsRequest("/extension/count?filter=name eq 'Telemetry Dashboard'", HTTPMethod.GET);
+			if (hasExtension1.Item1 != HttpStatusCode.OK)
+			{
+				return ActionResult.Failure;
+			}
+
+			if (JObject.Parse((string)JsonConvert.DeserializeObject(hasExtension1.Item2))["value"].ToObject<int>() == 0)
+			{
+				Tuple<HttpStatusCode, string> importExtensionResponse = MakeQrsRequest("/extension/upload", HTTPMethod.POST, HTTPContentType.json, Properties.Resources.Telemetry_Dashboard);
+				if (importExtensionResponse.Item1 != HttpStatusCode.Created)
+				{
+					return ActionResult.Failure;
+				}
+			}
+
+			// Extension 4
+			Tuple<HttpStatusCode, string> hasExtension4 = MakeQrsRequest("/extension/count?filter=name eq 'Telemetry Dashboard'", HTTPMethod.GET);
+			if (hasExtension1.Item1 != HttpStatusCode.OK)
+			{
+				return ActionResult.Failure;
+			}
+
+			if (JObject.Parse((string)JsonConvert.DeserializeObject(hasExtension1.Item2))["value"].ToObject<int>() == 0)
+			{
+				Tuple<HttpStatusCode, string> importExtensionResponse = MakeQrsRequest("/extension/upload", HTTPMethod.POST, HTTPContentType.json, Properties.Resources.Telemetry_Dashboard);
+				if (importExtensionResponse.Item1 != HttpStatusCode.Created)
+				{
+					return ActionResult.Failure;
+				}
+			}
 
 			return ActionResult.Success;
 		}
+
+		private static X509Certificate2 GetCertificate()
+		{
+			var clientPem = System.IO.File.ReadAllText(@"C:\ProgramData\Qlik\Sense\Repository\Exported Certificates\.Local Certificates\client.pem");
+			var clientKeyPem = System.IO.File.ReadAllText(@"C:\ProgramData\Qlik\Sense\Repository\Exported Certificates\.Local Certificates\client_key.pem");
+			byte[] certBuffer = HelperFunctions.GetBytesFromPEM(clientPem, Helpers.PemStringType.Certificate);
+			byte[] certKeyBuffer = HelperFunctions.GetBytesFromPEM(clientKeyPem, Helpers.PemStringType.RsaPrivateKey);
+
+			X509Certificate2 cert = new X509Certificate2(certBuffer);
+
+			RSACryptoServiceProvider provider = Crypto.DecodeRsaPrivateKey(certKeyBuffer);
+			cert.PrivateKey = provider;
+			return cert;
+		}
+
+
 	}
 }
